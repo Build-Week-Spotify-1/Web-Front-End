29b72f3be5c19c66f26618bb0f1060bf5f494a01
david-pok <david.pok@gmail.com>
2020-03-03 22:09:41 -0800

    some fixes

--- song-picker-front-end/src/components/DashBoard.js
+++ song-picker-front-end/src/components/DashBoard.js
@@ -1,29 +1,136 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import {
+  AppBar,
+  Button,
+  Grid,
+  Toolbar,
+  Typography,
+  Container,
+  Link
+} from "@material-ui/core/";
+import CameraIcon from "@material-ui/icons/PhotoCamera";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+import DashboardCards from "./DashboardCards";
 
-export default function DashBoard() {
-  const [search, setSearch] = useState("");
-  const [results, setResults] = useState([]);
-
-  const handleChange = e => {
-    setSearch(e.target.value);
-  };
-
+function Copyright() {
   return (
-    <section className="search-form">
-      Search For Songs Here
-      <form>
-        <label>
-          Artist/Song:
-          <input
-            id="name"
-            type="text"
-            name="textfield"
-            placeholder="Search"
-            value={search}
-            onChange={handleChange}
-          />
-        </label>
-      </form>
-    </section>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2)
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4)
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column"
+  },
+  cardMedia: {
+    paddingTop: "56.25%" // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6)
+  }
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function Album() {
+  const classes = useStyles();
+  const userMessage = localStorage.getItem("message");
+  return (
+    <div>
+      <CssBaseline />
+
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              {userMessage}
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don&apos;t simply skip over it entirely.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Main call to action
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <DashboardCards key={card} />
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </div>
   );
 }
